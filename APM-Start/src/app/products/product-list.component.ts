82f6bc0560380227dc2f907c86ffbeaf109c783c
Jasper Mollema <jasper.mollema@gmail.com>
2021-05-08 07:58:18 +0200

    Add Back button

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Product} from './product';
 import {ProductService} from './product.service';
-import {Subscription} from "rxjs";
+import {Subscription} from 'rxjs';
 
 @Component({
   templateUrl: './product-list.component.html',
